4c9b224c74e493b53db11adba81625405e25911e
liaojiancheng <liaojiancheng180@163.com>
2019-07-07 23:32:56 +0800

    commit aliyun_mqtt_app folder

---
+++ aliyun_mqtt_app/include/user_config.h
@@ -0,0 +1,13 @@
+#ifndef __USER_CONFIG_H__
+#define __USER_CONFIG_H__
+
+#define PRODUCT_KEY     "PRODUCT_KEY"
+
+#define DEVICE_NAME     "DEVICE_NAME"
+#define DEVICE_SECRET   "DEVICE_SECRET"
+
+#define WIFI_SSID       "WIFI_SSID"
+#define WIFI_PASS       "WIFI_PASS"
+
+#endif
+
